c1dce02188185d0acf31c5404a72ed7fb5fae549
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-01 14:15:25 +0530

    frontend

---
+++ frontend/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Add a click event listener to the button
+    document.getElementById('clickMe').addEventListener('click', function() {
+      alert('Button clicked!');
+    });
+  });
+  
